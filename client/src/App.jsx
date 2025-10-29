0581bce036e9e751422c1ed39d1454f91172d883
Yahya Al-Ademi <yahyaalademi84@gmail.com>
2025-10-29 09:13:44 +0100

    Add SearchInput, AlertMessage and HomePage components with styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Nav from "./components/Nav";
 import Home from "./pages/Home/Home";
 import CreateUser from "./pages/User/CreateUser";
 import UserList from "./pages/User/UserList";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/user" element={<UserList />} />
         <Route path="/user/create" element={<CreateUser />} />
       </Routes>
+      <HomePage />
     </>
   );
 };
