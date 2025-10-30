f485d88e04573a0d428ec35ef85c11d6daceeff6
Hanna Dubyna <ann10062016@gmail.com>
2025-10-30 22:04:25 +0100

    organize image import into separate module, create header and styles to it

---
+++ client/src/assets/index.js
@@ -0,0 +1,15 @@
+//images
+import defaultAvatar from "./images/default-avatar.png";
+import logo from "./images/logo.svg";
+
+//icons
+import arrow from "./icons/arrow.svg";
+
+export const images = {
+  defaultAvatar: defaultAvatar,
+  logo: logo,
+};
+
+export const icons = {
+  arrow: arrow,
+};
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,89 @@
+import "./Header.css";
+import { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { images } from "../../assets";
+import { icons } from "../../assets";
+
+//dropdown menu
+function UserMenu() {
+  const [open, setOpen] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const onDocClick = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) setOpen(false);
+    };
+    document.addEventListener("mousedown", onDocClick);
+    return () => document.removeEventListener("mousedown", onDocClick);
+  }, []);
+  const toggle = () => setOpen((v) => !v);
+
+  return (
+    <div className="user-menu" ref={ref}>
+      <button
+        type="button"
+        className="user-trigger"
+        onClick={toggle}
+        aria-expanded={open}
+        aria-haspopup="menu"
+      >
+        <img
+          src={images.defaultAvatar}
+          alt="user avatar"
+          className="user-avatar"
+        />
+        <span className="divider"></span>
+        <span className="user-name">Guest</span>
+
+        <img
+          src={icons.arrow}
+          alt=""
+          className={`arrow ${open ? "arrow-open" : ""}`}
+        />
+      </button>
+
+      {open && (
+        <div className="user-dropdown" role="menu">
+          <Link to="/profile" className="user-item" role="menuitem">
+            Profile
+          </Link>
+          <Link to="/about" className="user-item" role="menuitem">
+            About
+          </Link>
+          <Link to="/login" className="user-item" role="menuitem">
+            Login
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default function Header() {
+  return (
+    <header className="app-header">
+      <nav className="header-nav">
+        <div className="brand">
+          <img src={images.logo} alt="logo" className="logo-image-header" />
+          <span className="logo-text">Job Compass</span>
+        </div>
+
+        <div className="nav-links">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/jobs" className="nav-link">
+            Job listing
+          </Link>
+          <Link to="/favorites" className="nav-link">
+            Favorites
+          </Link>
+        </div>
+
+        <div className="header-actions">
+          <UserMenu />
+        </div>
+      </nav>
+    </header>
+  );
+}
