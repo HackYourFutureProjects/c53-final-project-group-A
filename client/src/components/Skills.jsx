987e14e5a8951bcf407f393cdfe4c60057df2e0c
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-11-02 22:41:09 +0100

    feat: add Skills component to display matched skills for job listings and update JobCard to use it style: add utility classes for Skills component in index.css refactor: remove unused skill match tag from JobCard

---
+++ client/src/components/Skills.jsx
@@ -0,0 +1,33 @@
+import { defaultUser } from "../data/defaultUser";
+
+function normalizeForWordSearch(str) {
+  return (" " + str + " ")
+    .replace(/[.*+?^${}()|[\]\\]/g, " ")
+    .replace(/\s+/g, " ")
+    .trim();
+}
+
+export default function Skills({ item }) {
+  const skills = defaultUser.skills;
+  function getSkillsInDescription(text) {
+    const textSpaced = normalizeForWordSearch(text);
+    return skills.filter((skill) => {
+      const re = new RegExp(" " + skill + " ", "i");
+      return re.test(textSpaced);
+    });
+  }
+  const skillsInDescription = getSkillsInDescription(item.descriptionText);
+
+  return (
+    <div className="flex gap-2">
+      <span className="text-sm font-medium mr-2">
+        Skills Match ({skillsInDescription.length}/{skills.length}):
+      </span>
+      {skillsInDescription.map((skill) => (
+        <span key={skill} className="text-xs bg-gray-100 px-2 py-1 rounded">
+          {skill}
+        </span>
+      ))}
+    </div>
+  );
+}
