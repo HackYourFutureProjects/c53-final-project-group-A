b51cf016142711db679391f7528fc0669642c42d
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-11-09 09:19:07 +0100

    Rename useFavorites hook to UseFavorites for consistency with naming conventions

--- client/src/pages/openPositions/OpenPositions.jsx
+++ client/src/pages/openPositions/OpenPositions.jsx
@@ -4,12 +4,12 @@ import JobCard from "../../components/JobCard/JobCard";
 import Pagination from "../../components/Pagination/Pagination";
 import { sortAndFilterJobs } from "../../util/sortingAndFiltering";
 import { UseJobs } from "../../context/JobsContext";
-import { useFavorites } from "../../context/FavoritesContext";
+import { UseFavorites } from "../../context/FavoritesContext";
 import "./OpenPositions.css";
 
 export default function OpenPositions() {
   const { allJobs, searchTerm, showResults } = UseJobs();
-  const { favorites, toggleFavorite } = useFavorites();
+  const { favorites, toggleFavorite } = UseFavorites();
 
   const [currentPage, setCurrentPage] = useState(1);
   const jobsPerPage = 5;
