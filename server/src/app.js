c082eced3e222728c0186bafc05861ad150b1c7b
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-11-16 16:12:49 +0100

    Format cors middleware for improved readability

--- server/src/app.js
+++ server/src/app.js
@@ -8,10 +8,12 @@ import dotenv from "dotenv";
 dotenv.config();
 // Create an express server
 const app = express();
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 // Tell express to use the json middleware
 app.use(express.json());
