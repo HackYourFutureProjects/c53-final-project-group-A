7e73dd49b5924bbda55191728ae49e48675e4802
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-11-21 20:28:27 +0100

    refactor: uncomment route handlers for travel time calculations

--- server/src/routes/travel.js
+++ server/src/routes/travel.js
@@ -1,11 +1,11 @@
 import express from "express";
-// import { calculateTravelTime } from "../controllers/travelController.js";
-// import calculateBatchTravelTime from "../controllers/travelController.js";
+import { calculateTravelTime } from "../controllers/travelController.js";
+import calculateBatchTravelTime from "../controllers/travelController.js";
 
 const router = express.Router();
 
-// router.post("/", calculateTravelTime);
+router.post("/", calculateTravelTime);
 
-// router.post("/batch", calculateBatchTravelTime);
+router.post("/batch", calculateBatchTravelTime);
 
 export default router;
