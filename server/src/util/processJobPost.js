96dfc7f04f7147d69c4ba2f7d2ce34abe147fd38
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-11-22 09:28:31 +0100

    refactor: update seniority normalization logic in processJobPost function

--- server/src/util/processJobPost.js
+++ server/src/util/processJobPost.js
@@ -1,7 +1,27 @@
 function normalizeDescription(str) {
   return " " + str.replace(/[^A-Za-z0-9+#]/g, " ").replace(/ +/g, " ") + " ";
 }
-
+// const senioritySynonyms = {
+//   "entry level": [
+//     "entry level",
+//     "entry-level",
+//     "junior",
+//     "instapniveau",
+//     "starter",
+//   ],
+//   associate: ["associate", "medewerker"],
+//   "mid-senior level": [
+//     "mid-senior",
+//     "mid senior",
+//     "medior",
+//     "senior",
+//     "ervaren",
+//   ],
+//   internship: ["internship", "intern", "stage", "stagiair"],
+//   director: ["director", "directeur", "hoofd"],
+//   executive: ["executive", "bestuurder", "c-level"],
+//   "not applicable": ["not applicable", "niet van toepassing", "n/a", "na"],
+// };
 export default function processJobPost(job) {
   const {
     id,
@@ -17,7 +37,33 @@ export default function processJobPost(job) {
     organization_url,
     organization_logo,
   } = job;
-
+  // normalize seniority values coming from the job source
+  let normalizedSeniority = seniority;
+  switch (seniority) {
+    case "Stagiair":
+      normalizedSeniority = "Internship";
+      break;
+    case "Instapniveau":
+      normalizedSeniority = "Entry level";
+      break;
+    case "Medewerker":
+      normalizedSeniority = "Associate";
+      break;
+    case "Senior medewerker":
+      normalizedSeniority = "Mid-Senior level";
+      break;
+    case "Directeur":
+      normalizedSeniority = "Director";
+      break;
+    case "Bestuurder":
+      normalizedSeniority = "Executive";
+      break;
+    case "Niet van toepassing":
+      normalizedSeniority = "n/a";
+      break;
+    default:
+      normalizedSeniority = seniority;
+  }
   return {
     id,
     url,
@@ -27,7 +73,7 @@ export default function processJobPost(job) {
     work_mode: remote_derived === true ? "Remote" : "On-site",
     display_location:
       locations_derived.length > 0 ? locations_derived[0] : null,
-    seniority: seniority === "Niet van toepassing" ? null : seniority,
+    seniority: normalizedSeniority,
     description_text,
     normalized_description:
       normalizeDescription(title) + normalizeDescription(description_text),
