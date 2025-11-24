65944dccd24d9adcfdafe360f273720fb40eaf83
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-11-24 11:48:13 +0100

    refactor: rename parameter sortCriteria to selectedSort for clarity in createSortComparator function

--- client/src/util/createSortComparator.js
+++ client/src/util/createSortComparator.js
@@ -1,16 +1,16 @@
-export default function createSortComparator(sortCriteria) {
+export default function createSortComparator(selectedSort) {
   return function compareObjects(jobA, jobB) {
     let i = 0;
     while (
-      i < sortCriteria.length - 1 &&
-      jobA[sortCriteria[i]] === jobB[sortCriteria[i]]
+      i < selectedSort.length - 1 &&
+      jobA[selectedSort[i]] === jobB[selectedSort[i]]
     ) {
       i++;
     }
-    if (i >= sortCriteria.length) {
-      i = sortCriteria.length - 1;
+    if (i >= selectedSort.length) {
+      i = selectedSort.length - 1;
     }
-    const key = sortCriteria[i];
+    const key = selectedSort[i];
     const a = jobA[key];
     const b = jobB[key];
     if (typeof a === "number" && typeof b === "number") {
