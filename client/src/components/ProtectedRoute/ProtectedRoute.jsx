6ddb24c5bb456c8658b016d803eac569453eee23
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-12-14 09:39:44 +0100

    refactor: remove loading state handling from ProtectedRoute component

--- client/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,12 +2,15 @@ import { Navigate } from "react-router-dom";
 import { UseUser } from "../../context/UserContext";
 
 export default function ProtectedRoute({ children }) {
-  const { user, loading: userLoading } = UseUser();
+  const {
+    user,
+    // , loading: userLoading
+  } = UseUser();
 
   // Show loading spinner while context is checking user (userLoading is TRUE initially)
-  if (userLoading) {
-    return <p>Loading...</p>;
-  }
+  // if (userLoading) {
+  //   return <p>Loading...</p>;
+  // }
 
   // This check ONLY runs AFTER userLoading is false.
   if (!user || !user.userid) {
